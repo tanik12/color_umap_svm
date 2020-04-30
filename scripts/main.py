978407b5cbf19e6eb0de8f6c3276087c781f40ea
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-04-30 12:07:41 +0900

    [update]ディレクトリ構成の変更とその変更に伴いコードの修正をした。

--- scripts/main.py
+++ scripts/main.py
@@ -1,92 +1,13 @@
-import numpy as np
-import pandas as pd
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-import umap
-import pickle
-from io import BytesIO
-from PIL import Image
-
-from skimage.color import rgb2lab
 import sys
+import numpy as np
+from skimage.color import rgb2lab
+from sklearn.model_selection import train_test_split
+
 from utils.data_loader import load_image
+from utils.ml_func import load_model, train_model, save_model, color_inference
+from utils.visualize import plot
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from mpl_toolkits.mplot3d import Axes3D
-
-
-def load_model(model_name):
-    try:
-        with open("../model/"+ model_name +".pickle", mode='rb') as fp:
-            clf = pickle.load(fp)
-            return clf
-    except FileNotFoundError as e:
-        print("Do not exist model file! Please make model file.", e)
-        sys.exit()
-
-def train_model(model_name, feature_space="None"):
-    try:
-        if 'umap' in model_name:
-            component_num = 3
-            trained_model = umap.UMAP(n_neighbors=80, n_components=component_num, min_dist=0.4, metric='cosine',random_state=12).fit(feature_space) #たぶんこれがよい
-        elif 'svm' in model_name:
-            trained_model = SVC(probability=True).fit(X_train, y_train)
-
-        return trained_model
-
-    except FileNotFoundError as e:
-        print("Do not exist model file! Please make model file.", e)
-        sys.exit()
-
-def save_model(model_name, train_obj):
-    with open("../model/" + model_name + ".pickle", mode='wb') as fp:
-        pickle.dump(train_obj, fp)
-
-def color_inference(x_train, model):
-    x_train = x_train.reshape(1, -1)
-    pred = model.predict(x_train)
-    label_name = label_dict[pred[0]]
-    
-    return pred, label_name
-
-def plot(tmp, label_arrays, component_num=3):
-    if component_num == 3:
-        #グラフの枠を作っていく
-        fig = plt.figure()
-        ax = Axes3D(fig)
-        
-        ax.set_xlabel("component1")
-        ax.set_ylabel("component2")
-        ax.set_zlabel("component3")
-
-        gif_flag = False
-        
-        for idx, label in enumerate(label_arrays):
-            label = label.astype(int)
-            if label == 0:
-                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="red", s = 10)
-            elif label == 1:
-                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="blue", s = 10)
-            elif label == 2:
-                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="yellow", s = 10)
-            else:
-                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="black", s = 10)
-
-        if gif_flag:
-            for angle in range(0, 180):
-                ax.view_init(30, angle*2)
-                plt.savefig("../pictures/figs/{0}_{1:03d}.jpg".format("res", angle))
-
-    elif component_num == 2:
-        plt.scatter(tmp.embedding_[:, 0], tmp.embedding_[:, 1], c=label_arrays, s = 10)
-
-    plt.show()
-
-
-if __name__ == '__main__':
+def main():
     ########################
     #input dataの作成
     #RGB
@@ -111,9 +32,9 @@ if __name__ == '__main__':
     ########################
     ######umapとsvmのobjectを作成
     # モデルの学習
-    trans = train_model("model_umap", feature_space=img_train)
+    trans = train_model("model_umap", train_data=img_train)
     X_train, X_test, y_train, y_test = train_test_split(trans.embedding_, label_arrays, test_size=0.4, random_state=12)
-    svc = train_model("model_svm")
+    svc = train_model("model_svm", train_data=X_train, tl_data=y_train)
 
     # 学習済みモデルを保存する
     save_model("model_umap", trans)
@@ -155,3 +76,5 @@ if __name__ == '__main__':
     print("SVM:", svc.score(X_test, y_test))
     ########################
 
+if __name__ == '__main__':
+    main()
---
+++ scripts/utils/visualize.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot(tmp, label_arrays, component_num=3):
+    if component_num == 3:
+        #グラフの枠を作っていく
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        
+        ax.set_xlabel("component1")
+        ax.set_ylabel("component2")
+        ax.set_zlabel("component3")
+
+        gif_flag = False
+        
+        for idx, label in enumerate(label_arrays):
+            label = label.astype(int)
+            if label == 0:
+                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="red", s = 10)
+            elif label == 1:
+                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="blue", s = 10)
+            elif label == 2:
+                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="yellow", s = 10)
+            else:
+                ax.scatter(tmp.embedding_[idx, 0], tmp.embedding_[idx, 1], tmp.embedding_[idx, 2], c="black", s = 10)
+
+        if gif_flag:
+            for angle in range(0, 180):
+                ax.view_init(30, angle*2)
+                plt.savefig("../pictures/figs/{0}_{1:03d}.jpg".format("res", angle))
+
+    elif component_num == 2:
+        plt.scatter(tmp.embedding_[:, 0], tmp.embedding_[:, 1], c=label_arrays, s = 10)
+
+    plt.show()
+
+if __name__ == "__main__":
+    pass
